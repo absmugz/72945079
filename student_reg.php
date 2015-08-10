5f1f795390cbf6a0382ebb932bb9ab0d72f876b2
absmugz <absmugz09@gmail.com>
2015-08-10 15:20:27 +0200

    styling the app using bootstrap

--- student_reg.php
+++ student_reg.php
@@ -1,33 +1,6 @@
-<?php
-/*------includes for connection to database-----*/
-include('includes/config.php');
-
-/*------includes for functions-----*/
-include('includes/functions.php');
-
-?>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-<title>Student registration system</title>
-<!-- Bootstrap -->
-<link href="css/bootstrap.min.css" rel="stylesheet">
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-<body>
-<div class="container">
-  <div class="row">
-    <div class="col-md-12">| <a href="student_reg.php">Register a student</a> | <a href="course_man.php">Manage courses</a> | <a href="student_man.php">Manage students</a> | <a href="list.php">View registrations</a></div>
-  </div>
-  <div class="row">
+<?php include("includes/header.php"); ?>
+<?php include("includes/navigation.php"); ?>
+<div class="row">
     <div class="col-md-12">
       <form class="form-horizontal">
         <div class="form-group">
@@ -68,56 +41,48 @@ include('includes/functions.php');
             <input type="radio" name="gender" id="inlineRadio2" value="option2">
             Female </label>
         </div>
-        
         <div class="form-group">
-    <label for="Language" class="col-sm-2 control-label">Language for correspondence</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="Language" placeholder="Language">
-    </div>
-  </div>
-  
-     <div class="form-group">
-    <label for="identity_number" class="col-sm-2 control-label">Identity Number</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="identity_number" placeholder="Identity Number">
-    </div>
-  </div>
-  
-  <div class="form-group">
-    <label for="home_number" class="col-sm-2 control-label">Home Telephone Code + Number</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="home_number" placeholder="Home Telephone Code + Number">
-    </div>
-  </div>
-  
-  <div class="form-group">
-    <label for="work_number" class="col-sm-2 control-label">Work Telephone Code + Number</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="work_number" placeholder="Work Telephone Code + Number">
-    </div>
-  </div>
-  
-  <div class="form-group">
-    <label for="cell_number" class="col-sm-2 control-label">Cell Phone Number</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="cell_number" placeholder="Cell Phone Number">
-    </div>
-  </div>
-        
+          <label for="Language" class="col-sm-2 control-label">Language for correspondence</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="Language" placeholder="Language">
+          </div>
+        </div>
         <div class="form-group">
-    <label for="Email" class="col-sm-2 control-label">Email</label>
-    <div class="col-sm-10">
-      <input type="email" class="form-control" id="Email" placeholder="Email">
-    </div>
-  </div>
-  
-  <div class="form-group">
-    <label for="postal_address" class="col-sm-2 control-label">Postal Address</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="postal_address" placeholder="Postal Address">
-    </div>
-  </div>
-  
+          <label for="identity_number" class="col-sm-2 control-label">Identity Number</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="identity_number" placeholder="Identity Number">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="home_number" class="col-sm-2 control-label">Home Telephone Code + Number</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="home_number" placeholder="Home Telephone Code + Number">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="work_number" class="col-sm-2 control-label">Work Telephone Code + Number</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="work_number" placeholder="Work Telephone Code + Number">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="cell_number" class="col-sm-2 control-label">Cell Phone Number</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="cell_number" placeholder="Cell Phone Number">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="Email" class="col-sm-2 control-label">Email</label>
+          <div class="col-sm-10">
+            <input type="email" class="form-control" id="Email" placeholder="Email">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="postal_address" class="col-sm-2 control-label">Postal Address</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="postal_address" placeholder="Postal Address">
+          </div>
+        </div>
         <div class="form-group">
           <div class="col-sm-offset-2 col-sm-10">
             <button type="submit" class="btn btn-default">Register Student</button>
@@ -126,10 +91,9 @@ include('includes/functions.php');
       </form>
     </div>
   </div>
-</div>
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="js/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
-</body>
-</html>
+
+<?php include("includes/footer.php"); ?>
+
+
+  
+  
