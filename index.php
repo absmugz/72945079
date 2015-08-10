d8bf3820595746c7f2bda15dae47cf60345e7c81
absmugz <absmugz09@gmail.com>
2015-08-10 13:27:57 +0200

    add css styling using bootstrap

--- index.php
+++ index.php
@@ -6,13 +6,26 @@ include('includes/config.php');
 include('includes/functions.php');
 
 ?>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Student registration system</title>
 
-<head>
-<link href="css/style.css" rel="stylesheet" type="text/css" />
-<title>Student registration system</title>
-</head>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-<div id="mainwrapper">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+<div class="container">
 
 
 
@@ -24,7 +37,9 @@ include('includes/functions.php');
 
 </div>
 
-</body>
-
-
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>
