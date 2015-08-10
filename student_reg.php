b839b20194dc77097c99f354c20a231eb57e4d7e
absmugz <absmugz09@gmail.com>
2015-08-10 17:10:34 +0200

    styling student reg form

--- student_reg.php
+++ student_reg.php
@@ -1,98 +1,118 @@
 <?php include("includes/header.php"); ?>
-<div class="row">
-    <div class="col-md-12">
+  <div class="row">
+    <div class="col-md-2"></div>
+    <div class="col-md-8">
       <form class="form-horizontal">
+      <div class="form-group">
+          <div class="row">
+            <div class="col-sm-4 text-right"><strong>Courses</strong></div>
+            <div class="col-sm-8">
+             <div class="check-radio-top"><label class="checkbox-inline">
+  <input type="checkbox" id="inlineCheckbox1" value="option1">Course A
+</label>
+<label class="checkbox-inline">
+  <input type="checkbox" id="inlineCheckbox2" value="option2">Course B
+</label>
+<label class="checkbox-inline">
+  <input type="checkbox" id="inlineCheckbox3" value="option3">Course C
+</label></div>
+
+            </div>
+          </div>
+        </div>
         <div class="form-group">
-          <label for="surname" class="col-sm-2 control-label">Surname</label>
-          <div class="col-sm-10">
+          <label for="surname" class="col-sm-4 control-label">Surname</label>
+          <div class="col-sm-8">
             <input type="text" class="form-control" id="surname" placeholder="Surname">
           </div>
         </div>
         <div class="form-group">
-          <label for="initials" class="col-sm-2 control-label">Initials</label>
-          <div class="col-sm-10">
+          <label for="initials" class="col-sm-4 control-label">Initials</label>
+          <div class="col-sm-8">
             <input type="text" class="form-control" id="initials" placeholder="Initials">
           </div>
         </div>
         <div class="form-group">
-          <label for="name" class="col-sm-2 control-label">Full First Name</label>
-          <div class="col-sm-10">
+          <label for="name" class="col-sm-4 control-label">Full First Name</label>
+          <div class="col-sm-8">
             <input type="text" class="form-control" id="name" placeholder="Full First Name">
           </div>
         </div>
         <div class="form-group">
-          <label for="title" class="col-sm-2 control-label">Title</label>
-          <div class="col-sm-10">
+          <label for="title" class="col-sm-4 control-label">Title</label>
+          <div class="col-sm-8">
             <input type="text" class="form-control" id="title" placeholder="Title">
           </div>
         </div>
         <div class="form-group">
-          <label for="dob" class="col-sm-2 control-label">Date of Birth</label>
-          <div class="col-sm-10">
+          <label for="dob" class="col-sm-4 control-label">Date of Birth</label>
+          <div class="col-sm-8">
             <input type="text" class="form-control" id="dob" placeholder="Date of Birth">
           </div>
         </div>
         <div class="form-group">
-          <label class="radio-inline">
-            <input type="radio" name="gender" id="inlineRadio1" value="option1">
-            Male </label>
-          <label class="radio-inline">
-            <input type="radio" name="gender" id="inlineRadio2" value="option2">
-            Female </label>
+          <div class="row">
+            <div class="col-sm-4 text-right"><strong>Gender</strong></div>
+            <div class="col-sm-8">
+             <div class="check-radio-top"> <label class="radio-inline">
+                <input type="radio" name="gender" id="inlineRadio1" value="option1">
+                Male </label>
+              <label class="radio-inline">
+                <input type="radio" name="gender" id="inlineRadio2" value="option2">
+                Female </label></div>
+            </div>
+          </div>
         </div>
         <div class="form-group">
-          <label for="Language" class="col-sm-2 control-label">Language for correspondence</label>
-          <div class="col-sm-10">
+          <label for="Language" class="col-sm-4 control-label">Language for correspondence</label>
+          <div class="col-sm-8">
             <input type="text" class="form-control" id="Language" placeholder="Language">
           </div>
         </div>
         <div class="form-group">
-          <label for="identity_number" class="col-sm-2 control-label">Identity Number</label>
-          <div class="col-sm-10">
+          <label for="identity_number" class="col-sm-4 control-label">Identity Number</label>
+          <div class="col-sm-8">
             <input type="text" class="form-control" id="identity_number" placeholder="Identity Number">
           </div>
         </div>
         <div class="form-group">
-          <label for="home_number" class="col-sm-2 control-label">Home Telephone Code + Number</label>
-          <div class="col-sm-10">
+          <label for="home_number" class="col-sm-4 control-label">Home Telephone Code + Number</label>
+          <div class="col-sm-8">
             <input type="text" class="form-control" id="home_number" placeholder="Home Telephone Code + Number">
           </div>
         </div>
         <div class="form-group">
-          <label for="work_number" class="col-sm-2 control-label">Work Telephone Code + Number</label>
-          <div class="col-sm-10">
+          <label for="work_number" class="col-sm-4 control-label">Work Telephone Code + Number</label>
+          <div class="col-sm-8">
             <input type="text" class="form-control" id="work_number" placeholder="Work Telephone Code + Number">
           </div>
         </div>
         <div class="form-group">
-          <label for="cell_number" class="col-sm-2 control-label">Cell Phone Number</label>
-          <div class="col-sm-10">
+          <label for="cell_number" class="col-sm-4 control-label">Cell Phone Number</label>
+          <div class="col-sm-8">
             <input type="text" class="form-control" id="cell_number" placeholder="Cell Phone Number">
           </div>
         </div>
         <div class="form-group">
-          <label for="Email" class="col-sm-2 control-label">Email</label>
-          <div class="col-sm-10">
+          <label for="Email" class="col-sm-4 control-label">Email</label>
+          <div class="col-sm-8">
             <input type="email" class="form-control" id="Email" placeholder="Email">
           </div>
         </div>
         <div class="form-group">
-          <label for="postal_address" class="col-sm-2 control-label">Postal Address</label>
-          <div class="col-sm-10">
+          <label for="postal_address" class="col-sm-4 control-label">Postal Address</label>
+          <div class="col-sm-8">
             <input type="text" class="form-control" id="postal_address" placeholder="Postal Address">
           </div>
         </div>
         <div class="form-group">
-          <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn btn-default">Register Student</button>
+        <div class="col-sm-4"></div>
+          <div class="col-sm-8">
+            <button type="submit" class="btn btn-primary">Register Student</button>
           </div>
         </div>
       </form>
     </div>
+    <div class="col-md-2"></div>
   </div>
-
-<?php include("includes/footer.php"); ?>
-
-
-  
-  
+  <?php include("includes/footer.php"); ?>
