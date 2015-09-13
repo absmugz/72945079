84f0f562cce453f7db4a2ae4d4bd6f7523a20856
absmugz <absmugz09@gmail.com>
2015-09-13 14:06:57 +0200

    style course selection table

--- list.php
+++ list.php
@@ -2,13 +2,15 @@
 <form name="filter" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">   
 <select name="courseselect">
       <option value="nothing" selected>Select course to show students</option>
-      <?php
-        while($row = mysqli_fetch_array($result_course)){
-    $id = $row['course_id'];
-    $name = $row['course_name'];
+<?php
+while($row = mysqli_fetch_array($result_course)){
+			
+$courseselect = $_POST['courseselect'];
+$id = $row['course_id'];
+$name = $row['course_name'];
 ?>
-        <option value="<?php echo $id; ?>" <?php if($id == $_POST['courseselect']) echo 'selected'; ?>><?php echo $name; ?></option>  
-    <?php
+<option value="<?php echo $id; ?>" <?php if($id == $courseselect) echo 'selected'; ?>><?php echo $name; ?></option>  
+<?php
 }
 ?>
  </select>
@@ -36,22 +38,17 @@ if (mysqli_num_rows($Course_StudentResult) == 0) {
 
 
 
-echo '<table>
-<thead>
-          <tr>
-            <th>#</th>
-            <th>Student Name</th>
+echo '<table width="100%" cellspacing="20" cellpadding="20">
+<tr>
+ <th>Student Name</th>
             <th>Delete</th>
           </tr>
-        </thead>
  <tbody>';
 		
 while($row = mysqli_fetch_array($Course_StudentResult)){
 ?>
           <tr>
-            <th scope="row"></th>
             <td><?php echo $row['student_fname'] . " " . $row['student_sname']?></td>
-            <td><a href="<?php echo $_SERVER['PHP_SELF'].'?edit_id='.$row['student_id']?>">Edit</a></td>
             <td><a href="<?php echo $_SERVER['PHP_SELF'].'?delete_id='.$row['student_id']?>">Delete</a></td>
           </tr>
           <?php
